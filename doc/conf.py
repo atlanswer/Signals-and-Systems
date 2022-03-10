6c0f340662e244ebdadc94727bdbdde3d4f9f191
Atlanswer <atlanswer@gmail.com>
2022-03-10 21:06:03 +0800

    content: fix submission time, release 0.1.2

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ author = 'X. Zhao'
 copyright = 'CC BY-NC-SA 4.0'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
