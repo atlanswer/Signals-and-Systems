f07f8a99c0b01c590a198a34a268d69fce4074b8
Atlanswer <atlanswer@gmail.com>
2022-04-14 20:49:45 +0800

    config: update dependencies, release 0.2.1

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ author = 'X. Zhao'
 copyright = 'CC BY-NC-SA 4.0'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
