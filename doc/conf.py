99f885a0f95cb4b3382c04026abd039d2716c440
Atlanswer <atlanswer@gmail.com>
2022-05-21 22:14:23 +0800

    config: fix MathJax CDN, migrate from jsDelivr to unpkg

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ author = 'X. Zhao'
 copyright = 'CC BY-NC-SA 4.0'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -54,6 +54,9 @@ extensions = [
 # MyST parser configuration
 myst_enable_extensions = ['dollarmath']
 myst_dmath_allow_labels = True
+
+# MathJax configuration
+mathjax_path = 'https://unpkg.com/mathjax@3/es5/tex-mml-chtml.js'
 
 # nbsphinx configuration
 nbsphinx_execute = 'never'
